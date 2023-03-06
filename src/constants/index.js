56f3939d48862563166646e3cfbf3720babf0e04
Vinayak Abhang <vinayal.abhang@crayoninfotech.com>
2023-03-06 12:09:43 +0530

    update folder files

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+import assets from './assets';
+import { COLORS, SIZES } from './theme'
+export { COLORS, SIZES, assets }
---
+++ src/constants/theme.js
@@ -0,0 +1,47 @@
+export const COLORS = {
+    brand: {
+        primary: "#1E3D73",
+        secondary: "#00833F",
+        background: "#F1F5F9",
+        fontcolor: '#36597D',
+        flap: '#1DAB87',
+    },
+    // neutrals: {
+    //     midnight: "#020f2f",
+    //     thunder: "#727c95",
+    //     cloud: "#b4bbce",
+    //     platinum: '#e5e8ef',
+    //     wine: '#9f1127',
+    //     peach: '#d09ea7',
+    //     candy: '#f7b9c4',
+    //     dawn: '#fce7ec',
+    //     charcoal: '#333333',
+    //     steel: '#666666',
+    //     silver: '#999999',
+    //     pearl: '#dddddd',
+    //     coconut: '#fafafa',
+    // },
+    // feedback: {
+    //     error: '#ff2c55',
+    //     caution: '#e6b800',
+    //     success: '#30b451',
+    //     information: '#006ee7',
+    //     errorBG: '#ffeff2',
+    //     cautionBG: '#fefbea',
+    //     successBG: '#effaf2',
+    //     informationBG: '#ebf5ff',
+    // }
+}
+
+export const SIZES = {
+    base: 10,
+    small: 12,
+    font: 14,
+    medium: 16,
+    large: 18,
+    mediumLarge: 24,
+    extraLarge: 28,
+    xl: 32,
+    xxl: 40,
+    xxxl: 80,
+}
